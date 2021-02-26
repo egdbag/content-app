98c175050c560bbf9775a031e8a9276f439a3c3c
tayou.sun <tayou.sun@gmail.com>
2021-02-26 23:05:44 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './components/Article/Article'
+import Article from './components/Article/Article';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Article></Article>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Article/Article.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function Article() {
+    return (
+        <div className="Article">
+
+            Hello world!
+        </div>
+    );
+}
+
+export default Article;
