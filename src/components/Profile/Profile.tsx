7aa3826704f35fc3fd0365fd1985045c0ad6b26c
Eugene Mavrin <eugene.mavrin@gmail.com>
2021-02-28 03:41:45 +0300

    Update Profile. Add achieve mock

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
-import DefaultProfilePic from './account_box-24px.svg'
+import Tooltip from '@material-ui/core/Tooltip';
+import Chip from '@material-ui/core/Chip';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import ShareIcon from '@material-ui/icons/Share';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import SearchIcon from '@material-ui/icons/Search';
+import StarIcon from '@material-ui/icons/Star';
 import './Profile.css';
 
 function ProfileMock() {
     return (
         <div className="profile">
-            <Avatar variant="rounded" alt="Mock"/>
-            <div className="profile__name">
-                <p>Firstname</p>
-                <p>Lastname</p>
+            <div className="profile__user">
+                <Avatar variant="rounded" alt="Mock"/>
+                <div className="profile__user__name">
+                    <p>Firstname</p>
+                    <p>Lastname</p>
+                </div>
+            </div>
+            <div className="profile__achieve">
+                <Tooltip title='Достижение: написать 5 статей'>
+                    <Chip className="profile__achieve__element" avatar={<Avatar><VisibilityIcon/></Avatar>} label="Оракул" color="secondary" />
+                </Tooltip>
+                <Tooltip title='Достижение: более 20 сохранений в избранное'>
+                    <Chip className="profile__achieve__element" avatar={<Avatar><BookmarkBorderIcon/></Avatar>} variant="outlined" label="Избранный" color="primary" />
+                </Tooltip>
             </div>
         </div>
     )
